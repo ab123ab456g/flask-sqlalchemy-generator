b470adecccc6e8f8b585dafef7531778c45803fe
YaoChiLu <ab123ab456g@YaoChiLude-MacBook-Pro.local>
2018-12-22 07:43:15 +0800

    finish test_DataType

--- flask_sqlalchemy_generator/test/test_models_prototypes.py
+++ flask_sqlalchemy_generator/test/test_models_prototypes.py
@@ -8,9 +8,20 @@ class TestDataType(unittest.TestCase):
 		test_dataType_Usually = ['Boolean', 'Integer', 'String' ]
 		self.assertEqual(DataType.dataType_Usually, test_dataType_Usually)
 
-	def test_dataType_Usually(self):
-		test_dataType_Usually = ['Boolean', 'Integer', 'String' ]
-		self.assertEqual(DataType.dataType_Usually, test_dataType_Usually)
+	def test_dataType_NonUsual(self):
+		test_dataType_NonUsaul = ['Integer', 'Array', 'BIGINT',
+        'BigINterger','BLOG', 'Boolean', 'BOOLEAN',
+        'CHAR', 'CLOB', 'Comparator', 'Concatenable',
+        'Date', 'DATE', 'Datetime', 'DATETIME', 'DECIMAL',
+        'ENUM', 'Float', 'Float', 'Interval', 'JSON', 
+        'JSONElementType', 'JSONIndexType', 'JSONPathType',
+        'LargeBinary', 'MatchType', 'NCHAR', 'NullType',
+        'Numeric', 'NUMBERIC', 'NVARCHAR',  'PickleType',
+        'REAL', 'SchemaType', 'SMALLINT', 'SmallInterger',
+        'String', 'TEXT', 'Text', 'Time', 'TIMESTAMP', 'TypeDecorator',
+        'TypeEngine', 'Unicode', 'UnicodeText' , 'UserDefinedType',
+        'VARBINARY','VARCHAR']
+		self.assertEqual(DataType.dataType_NonUsaul, test_dataType_NonUsaul)
 
 
 if __name__ == '__main__':
