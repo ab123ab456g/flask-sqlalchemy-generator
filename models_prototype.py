4948df7486edc5088b9eaa507944ef1f08d275b4
YaoChiLu <ab123ab456g@YaoChiLude-MacBook-Pro.local>
2018-12-22 04:04:32 +0800

    add test_models_prototypes.py and modify models_prototype.py add class DataType its about All DataType can chooise in SQLAlchemy

--- models_prototype.py
+++ models_prototype.py
@@ -1,4 +1,19 @@
-DataTypeList = ['Boolean', 'Integer', 'BigInterger', ]
+class Datatype:
+    dataType_Usually = ['Boolean', 'Integer', 'String' ]
+    dataType_NonUsaul = ['Integer', 'Array', 'BIGINT',
+        'BigINterger','BLOG', 'Boolean', 'BOOLEAN',
+        'CHAR', 'CLOB', 'Comparator', 'Concatenable',
+        'Date', 'DATE', 'Datetime', 'DATETIME', 'DECIMAL',
+        'ENUM', 'Float', 'Float', 'Interval', 'JSON', 
+        'JSONElementType', 'JSONIndexType', 'JSONPathType',
+        'LargeBinary', 'MatchType', 'NCHAR', 'NullType',
+        'Numeric', 'NUMBERIC', 'NVARCHAR',  'PickleType',
+        'REAL', 'SchemaType', 'SMALLINT', 'SmallInterger',
+        'String', 'TEXT', 'Text', 'Time', 'TIMESTAMP', 'TypeDecorator',
+        'TypeEngine', 'Unicode', 'UnicodeText' , 'UserDefinedType',
+        'VARBINARY', 'VARCHAR']
+
+
 class Patameter(object):
     def __init__(self):
         self.ParameterName = ''
