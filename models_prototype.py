2e9173ec925180e76eed20f54f57b7ec21386c83
YaoChiLu <ab123ab456g@YaoChiLude-MacBook-Pro.local>
2018-12-21 14:17:31 +0800

    add some code ,but not vary good ideal

---
+++ models_prototype.py
@@ -0,0 +1,18 @@
+DataTypeList = ['Boolean', 'Integer', 'BigInterger', ]
+class Patameter(object):
+    def __init__(self):
+        self.ParameterName = ''
+        self.DataType = ''
+        self.NotNull = True
+        self.CheckCondition = ''
+        self.PrimaryKey = True
+        self.ForiegnKey = ''
+
+
+
+class Prototype(object):
+    def __init__(self):
+        self.Name = ''
+        self.Parameter = ''
+        self.Parameter1Setti
+        ng = {'DataType':'int', 'Not Null':True, }
